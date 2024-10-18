11279922ce18fbfb0a9662b77e240c33d8aae394
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-10-18 18:05:03 +0530

    - Added local font and updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,26 @@ const faktumBold = localFont({
 	variable: "--font-faktum-bold",
 	weight: "100 900",
 });
+const generalSansBold = localFont({
+	src: "./fonts/GeneralSans-Bold.otf",
+	variable: "--font-generalsans-bold",
+	weight: "100 900",
+});
+const generalSansLight = localFont({
+	src: "./fonts/GeneralSans-Light.otf",
+	variable: "--font-generalsans-light",
+	weight: "100 900",
+});
+const generalSansMedium = localFont({
+	src: "./fonts/GeneralSans-Medium.otf",
+	variable: "--font-generalsans-medium",
+	weight: "100 900",
+});
+const generalSansRegular = localFont({
+	src: "./fonts/GeneralSans-Regular.otf",
+	variable: "--font-generalsans-regular",
+	weight: "100 900",
+});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -27,10 +47,15 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${faktumRegular.variable} ${faktumBold.variable} antialiased`}
+				className={`bg-white ${faktumRegular.variable} ${faktumBold.variable} ${generalSansBold.variable} ${generalSansLight.variable} ${generalSansMedium.variable} ${generalSansRegular.variable} antialiased`}
 			>
 				<Navigation />
-				{children}
+				<div
+					id="home"
+					className="m-8 min-h-screen flex flex-col w-auto bg-white"
+				>
+					{children}
+				</div>
 			</body>
 		</html>
 	);
