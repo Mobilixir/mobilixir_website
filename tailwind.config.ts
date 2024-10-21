7a99040bde1756e210018d087b658730a24f9487
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-10-21 19:49:13 +0530

    - Added animation in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,6 +31,21 @@ const config: Config = {
 				stormy: "#323334",
 				smoke: "#4E4F51",
 			},
+			keyframes: {
+				slidein: {
+					from: {
+						opacity: "0",
+						transform: "translateY(-10px)",
+					},
+					to: {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
+				},
+			},
+			animation: {
+				slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+			},
 		},
 	},
 	plugins: [],
