4034c7986ccf499a49b53b9e1de729b1a49426e7
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-10-23 18:07:48 +0530

    - Updated landing page

--- src/app/landingPage/choose.tsx
+++ src/app/landingPage/choose.tsx
@@ -1,27 +1,50 @@
+import Image from "next/image";
 import { info } from "../info/Info";
 function ChooseUs() {
 	return (
 		<div
 			id="why-choose-us"
-			className="h-screen w-auto container flex flex-col mx-auto justify-center items-center"
+			className="container mx-auto justify-center items-center basis-1 mt-10"
 		>
-			<h1 className="text-5xl font-bold text-black">
+			<h1 className="text-5xl font-bold text-black justify-center flex">
 				{info.landingPage.whychooseus.title}
 			</h1>
-			<div className=" mt-10">
-				<ul>
-					{info.landingPage.whychooseus.reasons.map((item) => {
-						return (
-							<li
-								className="text-black text-3xl mt-4 animate-slidein opacity-0 [--slidein-delay:700ms]"
-								key={item}
-							>
-								{" "}
-								&bull; {item}
-							</li>
-						);
-					})}
-				</ul>
+			<div className=" mt-10 flex flex-row justify-between w-auto">
+				<div className=" items-center justify-center flex">
+					<ul>
+						{info.landingPage.whychooseus.reasons.map((item) => {
+							return (
+								<li
+									className="text-black text-3xl mt-4 animate-slidein opacity-0 [--slidein-delay:700ms]"
+									key={item}
+								>
+									{" "}
+									&bull; {item}
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<div className="max-lg:hidden">
+					<Image
+						src="/one.svg"
+						alt="Logo"
+						width={0}
+						height={0}
+						className=" object-cover h-auto w-auto"
+					/>
+				</div>
+			</div>
+			<div className="flex-row flex justify-between items-center h-40 mt-16">
+				<p className="font-faktumBold-900 text-4xl max-lg:text-2xl text-black">
+					{info.landingPage.whychooseus.ctatitle}
+				</p>
+				<button
+					type="button"
+					className="min-lg:h-14 h-20 p-4 rounded-full bg-black"
+				>
+					{info.landingPage.ctaButton}
+				</button>
 			</div>
 		</div>
 	);
