5a133bf392df31e556c6ad5a58a949dd9133b73d
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-11-04 20:31:05 +0530

    - Updated nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
