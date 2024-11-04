61f65945231805e3a19d3c0fa8d6bf8ef3959d1b
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2024-11-04 19:01:17 +0530

    Feature/process (#6)
    * - Added infor for process
    
    * - Added ui for process

---
+++ src/app/process/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { info } from "../info/Info";
+function Process() {
+  return (
+    <div className="container mx-auto justify-center items-center basis-1 mt-10 animate-slidein opacity-0 [--slidein-delay:700ms]">
+      <h1 className="font-bold text-black font-faktumBold-900 text-5xl mb-10">
+        {info.process.title}
+      </h1>
+      {info.process.processList.map((process, index) => {
+        const css =
+          index % 2 == 0
+            ? "justify-start items-start"
+            : "justify-end items-end";
+        return (
+          <div key={index}>
+            <div className={`flex flex-col ${css} p-4 h-full w-full`}>
+              <p className="text-black font-faktumBold font-bold text-2xl">
+                {process.title}
+              </p>
+              <div className="h-auto">
+                <div className="text-black font-generalSansMedium text-base  w-auto animate-slidein opacity-0 [--slidein-delay:700ms]">
+                  {process.description}
+                </div>
+              </div>
+            </div>
+            <div className="h-0.5 bg-stone-500 w-auto" />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Process;
